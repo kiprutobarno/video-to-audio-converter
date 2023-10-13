9aef37e74ff389283984724a8127a0c72bef27e3
ywalakamar <barxwells@gmail.com>
2023-10-13 18:22:15 +0300

    build(docker): build and deploy with docker
    - write docker config files
    - write kubernetes config files in the `manifest` folder

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -6,7 +6,12 @@ from os import environ
 
 app = Flask(__name__)
 mysql = MySQL(app)
-app.config.from_envvar("APP_SETTINGS")
+# app.config.from_envvar("APP_SETTINGS")
+app.config["MYSQL_HOST"] = environ.get("MYSQL_HOST")
+app.config["MYSQL_USER"] = environ.get("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = environ.get("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = environ.get("MYSQL_DB")
+app.config["MYSQL_PORT"] = environ.get("MYSQL_PORT")
 
 
 @app.route("/login", methods=["POST"])
